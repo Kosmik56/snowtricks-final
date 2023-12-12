f0ebf9455d786d01a79e4a0577a6989a46f37248
Kosmik56 <lewis.kidd@gmail.com>
2023-12-12 15:08:36 +0100

    added tricks to FP

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TrickRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home_index')]
-    public function index(): Response
+    public function index(TrickRepository $trickRepository): Response
     {
+        $tricks = $trickRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'tricks' => $tricks,
+            
         ]);
     }
 }
